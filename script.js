ea05e12f958c54e6f7c519bcae9ea3acdd424e51
jaymoh23 <jeffreyryanmurray@gmail.com>
2017-09-21 17:46:29 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*Nav Bar*/
+$(".button-collapse").sideNav();
+
+
+
+/*Carousel*/
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+
+/*Parallax*/
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+      
+
